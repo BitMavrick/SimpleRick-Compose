652e7ee899654e4800fa31787dbb10ae32624729
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-02-08 22:37:38 +0600

    Setting up the other module

--- app/src/main/java/com/bitmavrick/simplerick/MainActivity.kt
+++ app/src/main/java/com/bitmavrick/simplerick/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.bitmavrick.network.TestFile
 import com.bitmavrick.simplerick.ui.theme.SimpleRickTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // Code from here ...
+                    TestFile()
                 }
             }
         }
---
+++ network/src/main/java/com/bitmavrick/network/TestFile.kt
@@ -0,0 +1,10 @@
+package com.bitmavrick.network
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun TestFile(){
+    Text(text = "Hello from other module")
+}
