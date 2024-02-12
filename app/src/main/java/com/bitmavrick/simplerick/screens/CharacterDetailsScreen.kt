d9b0bb5c3c7b1cfd285f5570927c56830de89a60
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-02-12 21:15:36 +0600

    Done with the custom button

--- app/src/main/java/com/bitmavrick/simplerick/screens/CharacterDetailsScreen.kt
+++ app/src/main/java/com/bitmavrick/simplerick/screens/CharacterDetailsScreen.kt
@@ -1,14 +1,18 @@
 package com.bitmavrick.simplerick.screens
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -18,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import com.bitmavrick.network.KtorClient
@@ -27,6 +33,7 @@ import com.bitmavrick.simplerick.components.character.CharacterDetailsNamePlateC
 import com.bitmavrick.simplerick.components.common.DataPoint
 import com.bitmavrick.simplerick.components.common.DataPointComponent
 import com.bitmavrick.simplerick.components.common.LoadingState
+import com.bitmavrick.simplerick.ui.theme.RickAction
 import kotlinx.coroutines.delay
 
 @Composable
@@ -99,5 +106,32 @@ fun CharacterDetailsScreen(
             Spacer(modifier = Modifier.height(32.dp))
             DataPointComponent(dataPoint = it)
         }
+
+        item { Spacer(modifier = Modifier.height(32.dp)) }
+
+        item{
+            Text(
+                text = "View all episodes",
+                color = RickAction,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(horizontal = 32.dp)
+                    .border(
+                        width = 1.dp,
+                        color = RickAction,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .clip(RoundedCornerShape(12.dp))
+                    .clickable {
+                        // Todo
+                    }
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth()
+
+            )
+        }
+
+        item { Spacer(modifier = Modifier.height(64.dp)) }
     }
 }
