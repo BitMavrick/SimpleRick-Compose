ff3c7e07e8ae5dca054184e734d512daed483b13
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-02-13 10:10:19 +0600

    Optimizing code

--- app/src/main/java/com/bitmavrick/simplerick/MainActivity.kt
+++ app/src/main/java/com/bitmavrick/simplerick/MainActivity.kt
@@ -4,21 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.bitmavrick.network.KtorClient
-import com.bitmavrick.network.models.domain.Character
 import com.bitmavrick.simplerick.screens.CharacterDetailsScreen
 import com.bitmavrick.simplerick.ui.theme.RickPrimary
 import com.bitmavrick.simplerick.ui.theme.SimpleRickTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
--- app/src/main/java/com/bitmavrick/simplerick/screens/CharacterDetailsScreen.kt
+++ app/src/main/java/com/bitmavrick/simplerick/screens/CharacterDetailsScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import com.bitmavrick.network.KtorClient
 import com.bitmavrick.network.models.domain.Character
